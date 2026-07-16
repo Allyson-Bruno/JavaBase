a2a659273513049fd3538ab6b599693c41be01c9
Allyson Bruno <allbruno2622@gmail.com>
2026-07-16 16:50:14 -0300

    Teste para ver o funcionamento

---
+++ src/all/introducaoappjava/HelloWorld.java
@@ -0,0 +1,13 @@
+package all.introducaoappjava;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		System.out.println("Hello World!");
+	}
+
+	public static void main(String[] args) {
+		new HelloWorld();
+	}
+
+}//class HelloWorld.
